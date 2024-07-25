5fd001a4019f44ae620ad299dadfb0108cdb5e65
Katty <katty.1996@mail.ru>
2024-07-25 16:18:46 +0300

    Добавлен сервис

--- src/main/java/com/example/botvk/BotVkApplication.java
+++ src/main/java/com/example/botvk/BotVkApplication.java
@@ -6,16 +6,16 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class BotVkZApplication implements CommandLineRunner {
+public class BotVkApplication implements CommandLineRunner {
 
     private final LongPollService longPollService;
 
-    public BotVkZApplication(LongPollService longPollService) {
+    public BotVkApplication(LongPollService longPollService) {
         this.longPollService = longPollService;
     }
 
     public static void main(String[] args) {
-        SpringApplication.run(BotVkZApplication.class, args);
+        SpringApplication.run(BotVkApplication.class, args);
     }
 
     @Override
